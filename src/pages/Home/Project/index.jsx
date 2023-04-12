f612ac75ef924c947b213ae8f01f5aa1eae5a6d0
TungLee0607 <letung31071999@gmail.com>
2023-04-12 14:14:31 +0700

    edit content project

--- src/pages/Home/Project/index.jsx
+++ src/pages/Home/Project/index.jsx
@@ -1,96 +1,94 @@
-import React from "react";
-import icon from "../../../assets/icons/icon-project.svg";
-import project1 from "../../../assets/images/projects/project1.png";
-import project2 from "../../../assets/images/projects/project2.png";
-import project3 from "../../../assets/images/projects/project3.png";
-import { useTranslation } from "react-i18next";
+import React from 'react'
+import icon from '../../../assets/icons/icon-project.svg'
+import project1 from '../../../assets/images/projects/project1.png'
+import project2 from '../../../assets/images/projects/project2.png'
+import project3 from '../../../assets/images/projects/project3.png'
+import { useTranslation } from 'react-i18next'
 
 const listProject = [
   {
     id: 1,
-    title: "VietPay",
+    title: 'VietPay',
     image: project1,
     content:
-      "Secure asset storage wallet and application to experience Rivius Network",
+      'Multi chain wallet allows storing cryptocurrencies in Rivius Network',
   },
   {
     id: 2,
-    title: "VietBox",
+    title: 'VietBox',
     image: project2,
     content:
-      "Real-time and data-connected applications in transport and logistics management",
+      'Real-time tracking and data transparency application in transport and logistics management',
   },
   {
     id: 3,
-    title: "TRASA",
+    title: 'TRASA',
     image: project3,
-    content: "Distributed database system and product traceability",
+    content: 'Distributed database system and product traceability',
   },
   {
     id: 4,
-    title: "Frowth",
+    title: 'Frowth',
     image: project1,
     content:
-      "Applying blockchain technology in Fintech to develop the financial system",
+      'Blockchain application in Fintech to develop the financial system',
   },
   {
     id: 5,
-    title: "Studemy",
+    title: 'Studemy',
     image: project2,
     content:
-      "Application to validate diplomas, certifications, training certificates and scientific background",
+      'Application to validate diplomas, certifications, training certificates, and scientific curriculum vitae',
   },
   {
     id: 6,
-    title: "Exam",
+    title: 'Exam',
     image: project3,
-    content: "Blockchain application in the management of exams",
+    content: 'Blockchain application in the management of exams',
   },
   {
     id: 7,
-    title: "Adoctic",
+    title: 'Adoctic',
     image: project1,
     content:
-      "Handbook of technology for storing medical records and genetic data",
+      'Handbook of technology for storing medical records and genetic data',
   },
   {
     id: 8,
-    title: "Tasker",
+    title: 'Tasker',
     image: project2,
-    content:
-      "Production management software. Easily track each step in the process",
+    content: 'Production management software. Easily tracking the process',
   },
   {
     id: 9,
-    title: "Kingstats",
+    title: 'Kingstats',
     image: project3,
     content:
-      "Accounting System - Auditing Rivius Blockchain application. Secure, look up, check data quickly",
+      'Accounting and Audit application. Time-saving on checking and comparing data',
   },
   {
     id: 10,
-    title: "Alvalink",
+    title: 'Alvalink',
     image: project1,
-    content: "Educational training quality assessment and management system",
+    content: 'Blockchain application in Education Management',
   },
   {
     id: 11,
-    title: "Walletia",
+    title: 'Walletia',
     image: project2,
-    content:
-      "Blockchain application supports domestic and international payment forms",
+    content: 'Blockchain application in Remittance & Money Transfer Services',
   },
   {
     id: 12,
-    title: "Nitrox",
+    title: 'Nitrox',
     image: project3,
     content:
-      "Derivatives exchange. Diversify investment portfolio, financial leverage",
+      'Derivatives exchange. Diversify investment portfolio, financial leverage',
   },
-];
+]
 
 export default function Project() {
-  const { t } = useTranslation();
+  const { t } = useTranslation()
 
   const renderListProject = () => {
     return listProject.map((item, index) => {
@@ -98,7 +96,7 @@ export default function Project() {
         <div className="project-home-item" key={`item: ${index}`}>
           <a
             className="project-home-item-image"
-            href={"/project?id=" + item.id}
+            href={'/project?id=' + item.id}
           >
             <img src={item.image} alt={item.title} />
           </a>
@@ -107,20 +105,20 @@ export default function Project() {
             <p>{t(item.content)}</p>
           </div>
         </div>
-      );
-    });
-  };
+      )
+    })
+  }
 
   return (
     <div className="project-home">
       <div className="project-home-title">
         <div className="title">
-          <h2>{t("Projects")}</h2>
+          <h2>{t('Projects')}</h2>
           <img src={icon} alt="icon" />
         </div>
         <p>
           {t(
-            "Rivius Network allows developers to launch decentralized applications (Defi, Gamefi, NFT), and supply chains or any financial products. Explore Dapps"
+            'Rivius Network allows developers to launch decentralized applications (Defi, Gamefi, NFT), and supply chains or any financial products. Explore Dapps',
           )}
         </p>
       </div>
@@ -132,5 +130,5 @@ export default function Project() {
         {renderListProject()}
       </div>
     </div>
-  );
+  )
 }
